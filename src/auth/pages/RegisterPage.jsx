1deaac8f0cf84b53169927b451ab41f294b151e0
Jorge Rivera <v-jrivera@catchpoint.com>
2022-10-28 23:03:30 -0500

    Journal App MUI

---
+++ src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,45 @@
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/authLayout";
+
+export const RegisterPage = () => {
+  return (
+    <AuthLayout title="Crear cuenta">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Tu nombre"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} >
+              <Button variant="contained" fullWidth>
+                Crear una cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{mr:1}}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
